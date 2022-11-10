f41bef4e2518f760cd42c2c85178f763d9ae54c6
agrey tbamanya <agreytibamanya04@gmail.com>
2022-11-10 05:56:49 +0300

    new folder added

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,154 @@
+import ButtonNew from './button/ButtonNew';
+import "../styles/buttons.css"
+export default function Buttons() {
+	return (
+		<div className='container'>
+
+			<div className='wrapper'>
+				<section>
+					<ButtonNew
+						id={'btn-one'}
+						type={'button'}
+						disabled={true}
+						variant={'contained'}
+						bcolor={'success'}
+						size={'medium'}
+						value={'new button'}
+					/>
+				</section>
+				<section>
+					<ButtonNew
+						id={'btn-two'}
+						type={'button'}
+						variant={'outlined'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+				</section>
+				<section>
+					<ButtonNew
+						id={'btn-three'}
+						type={'button'}
+						variant={'text'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+				</section>
+				<section>
+					<ButtonNew
+						id={'btn-four'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+				</section>
+
+				<section>
+					<ButtonNew
+						id={'btn-five'}
+						type={'button'}
+						isDisabled={true}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+					<ButtonNew
+						id={'btn-six'}
+						type={'button'}
+						isDisabled={true}
+						variant={'text'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+				</section>
+				<section>
+					<ButtonNew
+						id={'btn-seven'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+						isStart={'true'}
+					/>
+					<ButtonNew
+						id={'btn-eight'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+						isEnd={'true'}
+					/>
+				</section>
+
+				<section>
+					<ButtonNew
+						id={'btn-nine'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'small'}
+						value={'new button'}
+					/>
+					<ButtonNew
+						id={'btn-ten'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+					<ButtonNew
+						id={'btn-ten'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'large'}
+						value={'new button'}
+					/>
+				</section>
+				<section>
+					<ButtonNew
+						id={'btn-eleven'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'success'}
+						size={'medium'}
+						value={'new button'}
+					/>
+					 <ButtonNew
+						id={'btn-twelve'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'primary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+				<ButtonNew
+						id={'btn-13'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'secondary'}
+						size={'medium'}
+						value={'new button'}
+					/>
+						<ButtonNew
+						id={'btn-13'}
+						type={'button'}
+						variant={'contained'}
+						bcolor={'error'}
+						size={'medium'}
+						value={'new button'}
+					/>
+				</section>
+			</div>
+		</div>
+	);
+}
---
+++ src/components/button/ButtonNew.jsx
@@ -0,0 +1,36 @@
+
+import { Button} from "@mui/material";
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+
+
+const ButtonNew = props => {
+ 
+   
+    const{id,isDisabled,type,bcolor,size,isStart,isEnd,value,variant}=props
+
+  return (
+    <Button
+      id={id}
+      type={type}
+      disabled={isDisabled}
+      variant={variant}
+      color={bcolor}
+          size={size}
+          startIcon={isStart ?<LocalGroceryStoreIcon/>:<></>}
+          endIcon={isEnd ? <LocalGroceryStoreIcon/>:<></>}
+      >
+          
+          {value}
+          
+    </Button>
+  );
+};
+
+Button.defaultProps = {
+  type: "button",
+  isEnd: false,
+  isStart: false,
+  isDisabled: false
+};
+
+export default ButtonNew;
